5dd13a97f5feb638249e12d886ec391c4523bf48
MishinNR <nik.mishin024@gmail.com>
2022-06-11 18:48:37 +0300

    test @RequestParam from GET request

--- src/main/java/ru/mishin/spring/controllers/FirstController.java
+++ src/main/java/ru/mishin/spring/controllers/FirstController.java
@@ -3,6 +3,7 @@ package ru.mishin.spring.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -12,10 +13,8 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
-    // Внедрение класса HttpServletRequest, для обработки параметра GET запроса
-    public String helloPage(HttpServletRequest request) {
-        String name = request.getParameter("name");
-        String surname = request.getParameter("surname");
+    public String helloPage(@RequestParam("name") String name,
+                            @RequestParam("surname") String surname) {
         System.out.println("Hello, " + name + " " + surname);
 
         return "first/hello";
