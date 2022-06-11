562acaaded06b498e4b0299e65241f2c58859b1a
MishinNR <nik.mishin024@gmail.com>
2022-06-11 18:39:54 +0300

    test HttpServletRequest from GET request

--- src/main/java/ru/mishin/spring/controllers/FirstController.java
+++ src/main/java/ru/mishin/spring/controllers/FirstController.java
@@ -4,13 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 // Добавление ко всем завпросам значения /first
 // http://localhost:8080/hello –> http://localhost:8080/first/hello
-//@RequestMapping("/first")
+@RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage() {
+    // Внедрение класса HttpServletRequest, для обработки параметра GET запроса
+    public String helloPage(HttpServletRequest request) {
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+        System.out.println("Hello, " + name + " " + surname);
+
         return "first/hello";
     }
 
