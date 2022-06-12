016c40b989cb5e351a014a239f556df70c0e5aa0
MishinNR <nik.mishin024@gmail.com>
2022-06-12 14:37:59 +0300

    create simple calculator

--- src/main/java/ru/mishin/spring/controllers/FirstController.java
+++ src/main/java/ru/mishin/spring/controllers/FirstController.java
@@ -31,4 +31,34 @@ public class FirstController {
     public String goodByePage() {
         return "first/goodbye";
     }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam(value = "a") int a,
+                             @RequestParam(value = "b") int b,
+                             @RequestParam(value = "action") String action,
+                             Model model) {
+        double result;
+
+        switch (action) {
+            case "multiplication":
+                result = a * b;
+                break;
+            case "division":
+                result = a / (double) b;
+                break;
+            case "subtraction":
+                result = a - b;
+                break;
+            case "addition":
+                result = a + b;
+                break;
+            default:
+                result = 0;
+                break;
+        }
+
+        model.addAttribute("result", result);
+
+        return "first/calculator";
+    }
 }
