99886e4dd6dfc173fadc625efdcf4ba9fa433dbf
MishinNR <nik.mishin024@gmail.com>
2022-06-14 13:27:05 +0300

    add methods create, edit, update, delete

--- src/main/java/ru/mishin/spring/controllers/PeopleController.java
+++ src/main/java/ru/mishin/spring/controllers/PeopleController.java
@@ -17,14 +17,14 @@ public class PeopleController {
         this.personDAO = personDAO;
     }
 
-    @GetMapping()//http://localhost:8080/people
+    @GetMapping()// http://localhost:8080/people
     public String index(Model model) {
         // Получим всех людей из DAO и предадим на отображение в представление
         model.addAttribute("people", personDAO.index());
         return "people/index";
     }
 
-    @GetMapping("/{id}") //http://localhost:8080/people/{id}
+    @GetMapping("/{id}") // http://localhost:8080/people/{id}
     // Аннотация @PathVariable извлекает id из URL-адреса
     public String show(@PathVariable("id") int id, Model model) {
         // Получим одного человека по его id из DAO и предадим на отображение в представление
@@ -32,17 +32,36 @@ public class PeopleController {
         return "people/show";
     }
 
-    @GetMapping("/new")
+    @GetMapping("/new") // http://localhost:8080/people/new
     // Аннотация @ModelAttribute("person") сама создаст объект класса Model и положит его в модель
     public String newPerson(@ModelAttribute("person") Person person) {
         return "people/new";
     }
 
-    @PostMapping()
+    @PostMapping()// http://localhost:8080/people <– POST
     public String create(@ModelAttribute("person") Person person) {
         // Если в форме ничего не будет записано в объект person,
         // то в модель будет записан объект со значениями по умолчанию
         personDAO.save(person);
+        return "redirect:/people";
+    }
+
+    @GetMapping("/{id}/edit") // http://localhost:8080/people/{id}/edit
+    public String edit(@PathVariable("id") int id, Model model) {
+        model.addAttribute("person", personDAO.show(id));
+        return "people/edit";
+    }
+
+    @PatchMapping("/{id}") // http://localhost:8080/people/{id} <– PATCH
+    public String update(@ModelAttribute("person") Person person,
+                         @PathVariable("id") int id) {
+        personDAO.update(id, person);
+        return "redirect:/people";
+    }
+
+    @DeleteMapping("/{id}") // http://localhost:8080/people/{id} <– DELETE
+    public String delete(@PathVariable("id") int id) {
+        personDAO.delete(id);
         return "redirect:/people";
     }
 }
